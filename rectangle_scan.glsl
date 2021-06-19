ad275fec36480bedd11772f47bcedbf55b39eb59
Atul T <atul0x60@gmail.com>
2021-06-19 15:40:39 +0530

    Rectangle Scan Line

--- rectangle_scan.glsl
+++ rectangle_scan.glsl
@@ -11,13 +11,20 @@ float plot(float y, float pct) {
     return smoothstep(pct-w/2.0, pct, y) - smoothstep(pct, pct+w/2.0, y);
 }
 
+float inside_rect(vec2 st) {
+    vec2 ro = vec2(0.4);
+    vec2 dm = vec2(0.2);
+    return (step(ro.x, st.x) - step(ro.x+dm.x, st.x)) * (step(ro.y, st.y) - step(ro.y+dm.y, st.y));
+}
+
 void main() {        
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    // map x axis [0.0, 1.0] to [0.0, 2*pi] to include a full sin wave
-    // and add u_time to animate.
-    float y = sin(st.x * 3.14159 * 2.0 + u_time);
-    // resize wave by half half and make it center
-    float pct = plot(st.y, y*0.25+0.5);
+    // get line
+    float y = abs(sin(u_time * 2.0));
+    // draw rectangle and show only if its position is above line
+    float pct = step(y, st.y) * inside_rect(st);
+    // mix rectangle and line
+    pct += plot(st.y, y);    
     vec3 color = vec3(0.0);
     color = (1.0-pct)*color+(pct)*vec3(0.0, 1.0, 0.0);
     gl_FragColor = vec4(color, 1.0);
